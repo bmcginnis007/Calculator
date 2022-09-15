1c247eb8c1f94d4a4edd534e7226b65e6d16aca7
Brian <technophile1974@gmail.com>
2022-09-15 04:43:46 -0400

    trying different functions to update display with button values

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function increment(){
       document.getElementById("btn7").value = 777;
     }
   }
+
+  function increment8(){
+    if (document.getElementById("btn7").value == ""){
+      document.getElementById("numerator").innerHTML= 8;
+      document.getElementById("btn7").value = 8;
+    }
+    else if (document.getElementById("btn7").value == 8){
+      document.getElementById("numerator").innerHTML= 88;
+      document.getElementById("btn7").value = 88;
+    }
+    else if (document.getElementById("btn7").value == 88){
+      document.getElementById("numerator").innerHTML= 888;
+      document.getElementById("btn7").value = 888;
+    }
+  }
