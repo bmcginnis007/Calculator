8c4bfdddd3080abbd610c393b4c91c6405083e30
Brian <technophile1974@gmail.com>
2022-09-20 05:05:13 -0400

    nothing much has changed. Still trying to create a fuction to populate the display with the correct numbers when clicking the buttons

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-function increment(){
-    if (document.getElementById("btn7").value == ""){
-      document.getElementById("numerator").innerHTML= 7;
-      document.getElementById("btn7").value = 7;
-    }
-    else if (document.getElementById("btn7").value == 7){
-      document.getElementById("numerator").innerHTML= 77;
-      document.getElementById("btn7").value = 77;
-    }
-    else if (document.getElementById("btn7").value == 77){
-      document.getElementById("numerator").innerHTML= 777;
-      document.getElementById("btn7").value = 777;
-    }
-  }
+function increment(a,b){
+   document.getElementById("numerator").innerHTML= a * 10 + b;
+}
 
   function increment8(){
     if (document.getElementById("btn7").value == ""){
