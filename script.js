0978e2c08a6df346b7473bd8714c734c1fb0cb75
Brian <technophile1974@gmail.com>
2022-09-25 08:47:21 -0400

    no change

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ let numberArray = [];
 
 function increment(a){
    numberArray.push(a);
-   document.getElementById("numerator").innerHTML= numberArray.join("");
-   console.log(typeof numberArray[0]);
+   document.getElementById("numbers").innerHTML= numberArray.join("");
 }
 
 function ce(){
    numberArray.pop();
-   document.getElementById("numerator").innerHTML= numberArray.join("");
+   document.getElementById("numbers").innerHTML= numberArray.join("");
 }
   
