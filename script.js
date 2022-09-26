cba9165cd7d8f4a9ccb190714378611cba95350e
Brian <technophile1974@gmail.com>
2022-09-26 04:59:20 -0400

    experimenting with functions

--- script.js
+++ script.js
@@ -9,4 +9,14 @@ function ce(){
    numberArray.pop();
    document.getElementById("numbers").innerHTML= numberArray.join("");
 }
-  
+
+const array1 = [7, 7, 7];
+const produceAndAdd = (array1 = []) => {
+   let sum = 0;
+   for(let i=0; i < array1.length; i++) {
+      const product = (array1[i] * 10 + 7);
+      sum += product;
+   };
+   return sum;
+};
+console.log(produceAndAdd(array1));
