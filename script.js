b1923621f3e44e92a3e0555d250327c38eea308a
Brian <technophile1974@gmail.com>
2023-02-08 05:13:50 -0500

    working on storing the first and second operands

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-let value = "";
-let value2 = "";
+let firstOperand = "";
+let secondOperand = "";
+var operands = [firstOperand, secondOperand];
 let buttons = document.querySelectorAll("button[id='num']");
 for (let i = 0; i < buttons.length; i++) {
    buttons[i].addEventListener("click", function() {
-      value += this.value;
-      value = parseFloat(value);
-      document.getElementById("numbers").innerHTML = value;
-      console.log(value);
-      console.log(typeof value);
+      operands[0] += this.value;
+      operands[0] = parseFloat(operands[0]);
+      document.getElementById("numbers").innerHTML = operands[0];
+      console.log(operands[0]);
+      console.log(typeof operands[0]);
    });
 }
 
-document.getElementById("add").addEventListener("click", function(){
-   document.getElementById("numbers").innerHTML = value + " +";
-   });
-
-
 function clearEntry(){
-   value = value.toString();
-   value = value.slice(0, -1);
-   value = parseFloat(value);
-   if(isNaN(value)){
-      value = 0;
+   operands[0] = operands[0].toString();
+   operands[0] = operands[0].slice(0, -1);
+   operands[0] = parseFloat(operands[0]);
+   if(isNaN(operands[0])){
+      operands[0] = 0;
    }
-   document.getElementById("numbers").innerHTML = value;
-   console.log(value);
-   console.log(typeof value);
+   document.getElementById("numbers").innerHTML = operands[0];
+   console.log(operands[0]);
+   console.log(typeof operands[0]);
 }
 
 function allClear(){
- value = 0;
- document.getElementById("numbers").innerHTML = value;
+ operands[0] = 0;
+ document.getElementById("numbers").innerHTML = operands[0];
 }
 
 function operate(a, operator, b){
